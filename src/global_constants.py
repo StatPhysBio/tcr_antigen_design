ffd597375b759c040abd49a84a6fbf74333cee37
Gian Marco Visani <gvisan01@uw.edu>
2025-01-06 19:53:43 +0100

    design plots

---
+++ src/global_constants.py
@@ -0,0 +1,28 @@
+
+LOGOMAKER_COLORS = {
+    # Nonpolar, nonaromatic (aliphatic + other)
+    'A': '#8CC055',
+    'G': '#8CC055',
+    'I': '#8CC055',
+    'L': '#8CC055',
+    'M': '#8CC055',
+    'V': '#8CC055',
+    'P': '#8CC055',
+    # Positively charged, polar, hydrophilic
+    'H': '#E85542',
+    'K': '#E85542',
+    'R': '#E85542',
+    # Negatively charged, polar, hydrophilic
+    'D': '#63A0E2',
+    'E': '#63A0E2',
+    # Nonpolar, neutral, aromatic
+    'F': '#7866B7',
+    'W': '#7866B7',
+    'Y': '#7866B7',
+    # Polar, uncharged
+    'C': '#FCCC5C',
+    'N': '#FCCC5C',
+    'Q': '#FCCC5C',
+    'S': '#FCCC5C',
+    'T': '#FCCC5C',
+}
