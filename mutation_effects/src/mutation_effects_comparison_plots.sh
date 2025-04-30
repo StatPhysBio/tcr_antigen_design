273edc83185025df167bed2ea3df07af6499fd18
Gian Marco Visani <gvisan01@uw.edu>
2025-04-30 11:18:53 -0700

    logos of hermes_ft_relaxed on pmhc

--- mutation_effects/src/mutation_effects_comparison_plots.sh
+++ mutation_effects/src/mutation_effects_comparison_plots.sh
@@ -5,17 +5,17 @@ python mutation_effects_comparison_plots.py --system tax --system_name_in_csv_fi
 
 python mutation_effects_comparison_plots.py --system hsiue_et_al --system_name_in_csv_file hsiue_et_al_H2_sat_mut
 
-# python mutation_effects_comparison_plots.py --system mskcc --system_name_in_csv_file mskcc_tcr1_ec50_sat_mut_af3
+python mutation_effects_comparison_plots.py --system mskcc --system_name_in_csv_file mskcc_tcr1_ec50_sat_mut_af3
 
-# python mutation_effects_comparison_plots.py --system mskcc --system_name_in_csv_file mskcc_tcr2_ec50_sat_mut_af3
+python mutation_effects_comparison_plots.py --system mskcc --system_name_in_csv_file mskcc_tcr2_ec50_sat_mut_af3
 
-# python mutation_effects_comparison_plots.py --system mskcc --system_name_in_csv_file mskcc_tcr3_ec50_sat_mut_af3
+python mutation_effects_comparison_plots.py --system mskcc --system_name_in_csv_file mskcc_tcr3_ec50_sat_mut_af3
 
-# python mutation_effects_comparison_plots.py --system mskcc --system_name_in_csv_file mskcc_tcr4_ec50_sat_mut_af3
+python mutation_effects_comparison_plots.py --system mskcc --system_name_in_csv_file mskcc_tcr4_ec50_sat_mut_af3
 
-# python mutation_effects_comparison_plots.py --system mskcc --system_name_in_csv_file mskcc_tcr5_ec50_sat_mut_af3
+python mutation_effects_comparison_plots.py --system mskcc --system_name_in_csv_file mskcc_tcr5_ec50_sat_mut_af3
 
-# python mutation_effects_comparison_plots.py --system mskcc --system_name_in_csv_file mskcc_tcr6_ec50_sat_mut_af3
+python mutation_effects_comparison_plots.py --system mskcc --system_name_in_csv_file mskcc_tcr6_ec50_sat_mut_af3
 
-# python mutation_effects_comparison_plots.py --system mskcc --system_name_in_csv_file mskcc_tcr7_ec50_sat_mut_af3
+python mutation_effects_comparison_plots.py --system mskcc --system_name_in_csv_file mskcc_tcr7_ec50_sat_mut_af3
 
