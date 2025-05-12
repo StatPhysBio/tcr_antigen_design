e4ee11aab3f2a5da72677420683d5e9c5f94b600
Gian Marco Visani <gvisan01@g3062.hyak.local>
2025-05-12 13:43:47 -0700

    nyeso design scores relaxed

--- peptide_designs/nyeso/nyeso_full_copy/gather_hermes_with_relaxation.sh
+++ peptide_designs/nyeso/nyeso_full_copy/gather_hermes_with_relaxation.sh
@@ -9,11 +9,11 @@ python ../../src/gather_score_with_relaxation_results.py \
                 --use_max_instead_of_mean 1 \
                 --output_in_inputfile 1
 
-# python ../../src/gather_score_with_relaxation_results.py \
-#                 --csv_filename hcnn_pyrosetta_annealing/hcnn_plus_pyrosetta_annealing_peptides_so3_convnet_noise=0p5_w_pae_w_blosum.tsv \
-#                 --pdbid from_csv \
-#                 --model_version hermes_py_050 \
-#                 --pdb_column pdb_for_hermes_scoring \
-#                 --seq_column peptide \
-#                 --output_in_inputfile 1
+python ../../src/gather_score_with_relaxation_results.py \
+                --csv_filename hcnn_pyrosetta_annealing/hcnn_plus_pyrosetta_annealing_peptides_so3_convnet_noise=0p5_w_pae_w_blosum.tsv \
+                --pdbid from_csv \
+                --model_version hermes_py_050 \
+                --pdb_column pdb_for_hermes_scoring \
+                --seq_column peptide \
+                --output_in_inputfile 1
 
